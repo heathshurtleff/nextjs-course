a9309cb71c8e09adb67f981114c45b06aeaf20ed
Heath Shurtleff <h2shurtleff@gmail.com>
2021-09-09 13:47:33 -0600

    Styled the event cards

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 
+import styles from './EventItem.module.css';
+
 const EventItem = (props) => {
   const {
     title,
@@ -20,19 +22,19 @@ const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={styles.item}>
       <img src={'/' + image} alt={title} />
-      <div>
-        <div>
+      <div className={styles.content}>
+        <div className={styles.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={styles.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={styles.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={styles.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/EventList.js
+++ components/events/EventList.js
@@ -1,10 +1,12 @@
 import EventItem from "./EventItem";
 
+import styles from './EventList.module.css';
+
 const EventList = (props) => {
   const { items } = props;
 
   return (
-    <ul>
+    <ul className={styles.list}>
       {items.map(item => <EventItem key={item.id} {...item} />)}
     </ul>
   )
